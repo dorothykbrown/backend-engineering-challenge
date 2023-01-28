17c1ee060785b2e14df0b2858adbe1f6744a8419
Dorothy Brown <43494893+dorothykbrown@users.noreply.github.com>
2023-01-28 12:04:07 +0000

    Create test_format_translation_results

--- test_avg_translation_time.py
+++ test_avg_translation_time.py
@@ -1,4 +1,5 @@
 import unittest
+from datetime import datetime
 from avg_translation_time import AverageTranslationTime
 
 class TestAverageTranslationTime(unittest.TestCase):
@@ -18,7 +19,60 @@ class TestAverageTranslationTime(unittest.TestCase):
         self.assertEqual(average, None, "average is not None")
 
     def test_format_translation_results(self):
-        pass
+
+        unformatted_translations = [
+            {
+                "timestamp": "2018-12-26 18:11:08.509654",
+                "translation_id": "5aa5b2f39f7254a75aa5",
+                "source_language": "en",
+                "target_language": "fr",
+                "client_name": "easyjet",
+                "event_name": "translation_delivered",
+                "nr_words": "30",
+                "duration": "20"
+            },
+            {
+                "timestamp": "2018-12-26 18:15:19.903159",
+                "translation_id": "5aa5b2f39f7254a75aa4",
+                "source_language": "en",
+                "target_language": "fr",
+                "client_name": "easyjet",
+                "event_name": "translation_delivered",
+                "nr_words": "30",
+                "duration": "31"
+            }
+        ]
+        
+        formatted_translations = AverageTranslationTime.format_translation_results(unformatted_translations)
+        
+        expected_formatted_translations = [
+            {
+                "timestamp": datetime(year=2018, month=12, day=26, hour=18, minute=11, second=8, microsecond=509654),
+                "translation_id": "5aa5b2f39f7254a75aa5",
+                "source_language": "en",
+                "target_language": "fr",
+                "client_name": "easyjet",
+                "event_name": "translation_delivered",
+                "nr_words": "30",
+                "duration": 20
+            },
+            {
+                "timestamp": datetime(year=2018, month=12, day=26, hour=18, minute=15, second=19, microsecond=903159),
+                "translation_id": "5aa5b2f39f7254a75aa4",
+                "source_language": "en",
+                "target_language": "fr",
+                "client_name": "easyjet",
+                "event_name": "translation_delivered",
+                "nr_words": "30",
+                "duration": 31
+            }
+        ]
+
+        self.assertEqual(
+            formatted_translations,
+            expected_formatted_translations,
+            "formatted translations are not as expected"
+        )
 
     def test_convert_string_to_datetime(self):
         pass
